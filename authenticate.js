71da40861136f335af4d96ac73624818b5260081
Aayush Bhankale <abhankale@gmail.com>
2020-10-02 20:38:22 +0530

    feat: Oauth implementation

--- authenticate.js
+++ authenticate.js
@@ -6,6 +6,7 @@ var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
 const config = require('./config');
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 passport.use(new localStratergy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -64,7 +65,7 @@ exports.verifyAdmin = (req, res, next) => {
     User.findById({_id: req.user._id})
     .then(user => {
         if(!user.admin) {
-            const err = new Error('You are not authorized to perform this operation!');
+            const err = new Error('Admin have authorized to perform this operation!');
             err.status = 401;
             return next(err);
         } else {
@@ -73,3 +74,30 @@ exports.verifyAdmin = (req, res, next) => {
     })
     .catch(err => next(err))
 };
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
+}
+));
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '337381870908448',
+        clientSecret: '3cda994b6b6ea40dced28ed646945e58'
+    }
 }
